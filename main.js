f7491e3ca7069492a30b2139bbf76259d9d6d6fc
gigalataria <latariagiga008@gmail.com>
2025-03-29 14:46:13 +0400

    Add files via upload

--- main.js
+++ main.js
@@ -1,1 +1,9 @@
-const b1 = document.querySelector(.b1)
+const button1 = document.querySelector(".b1")
+const elem1 = document.querySelector("body")
+const elem2 = document.querySelector(".h2")
+
+button1.addEventListener("click", () => {
+    button1.style.backgroundColor = "purple"
+    elem1.style.backgroundImage = "url(5de.jpg)"
+    elem2.textContent = "WIZARDS"
+})
